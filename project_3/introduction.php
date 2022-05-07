0eb2aefe47b9c32b5c95cbe8f5c429cc80bd45bf
unknown <jason2133@likelion.org>
2022-05-08 02:20:35 +0900

    project

---
+++ project_3/introduction.php
@@ -0,0 +1,105 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+<title>고려대학교 KU메이커스페이스</title>
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/default.css">
+<link rel="shortcut icon" href="images/ku_logo.png">
+<!-- <link rel="apple-touch-icon-precomposed" href="images/favicon/indigo-touch-icon.png"> -->
+<link rel="apple-touch-icon-precomposed" href="images/ku_logo.png">
+<script src="js/jquery.min.js"></script>
+<script src="js/indigo.min.js"></script>
+</head>
+<body>
+    <div id="wrap">
+        <header class="header cfixed">
+            <h1 class="logo"><a href="index.php">KU Makerspace</a></h1>
+            <nav>
+                <ul class="gnb">
+                    <li><a href="introduction.php">기관 소개</a></li>
+                    <li><a href="education.php">장비교육</a></li>
+                    <li><a href="reservation.php">장비예약</a></li>
+                    <li><a href="notice.php">공지사항</a></li>
+                    <li><a href="freeboard.php">자유 게시판</a></li>
+                    <li><a href="product.php">대회 작품들</a></li>
+                    <li><a href="login/login_page.php">회원가입/로그인</a></li>
+                </ul>
+            </nav>
+            <span class="menu-toggle-btn">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
+        </header>
+        <!-- <article class="slider"> -->
+            <!-- <img src="images/p-images/slide01.jpg" alt=""> -->
+            <!-- <img src="images/kumakerspace_space.jpg" alt="">/ -->
+        <!-- </article> -->
+        <section class="content">
+            <section class="display-section">
+                <div class="container">
+                    <h2 class="sec-tit">Welcome!</h2>
+                    <p class="desc">내가 원하는 물건을 직접 설계하고 제작해볼 수 있는 공간</br>고려대학교 KU메이커스페이스에 오신 것을 환영합니다.</p>
+                </div>
+            </section>
+            <p class="introhangeul1">안녕하세요, <span class='spanred'>KU Makerspace 소장</span> ㅇㅇㅇ입니다.</br>
+                저희 KU Makerspace는 2018년 7월 창의관 1층에 개소하였습니다.</br></br>
+                3D 프린터, 레이저커터, 티셔츠 프린트기, 프로그램 자수기, CNC, 목공실 등 다양한 최신 제작툴을 구비하고 있습니다.</br>
+                전문 트레이너들을 통해 정기적인 메이커 교육을 실시하여 제작 전문성 재고에도 많은 노력을 기울이고 있습니다.</br>
+                해커톤, 제작역량강화교육, 네트워킹행사 등을 통해 메이커 무브먼트(Maker Movement) 문화 확산에도 힘쓰고 있습니다.</br></br>
+                KU Makersapce는 “고려대학교 구성원”이라면 누구나 이용할 수 있으며 원하는 제품을 직접 디자인하고 만들어 볼 수 있는 공간입니다.</br>
+                또한 필요한 환경과 교육을 모두 제공할 수 있도록 노력하고 있습니다.</br></br>
+                많은 관심과 참여 부탁드립니다. 감사합니다.</br>
+            </p>
+            <p class="introhangeul2"><span class='spanred'>KU Makersapce 소장</span> ㅇㅇㅇ 드림</p>
+            <section class="display-section">
+            <article class="intro1">
+                <img src="images/kumakerspace_space2.jpg" alt="">
+            </article>
+            <article class="intro2">
+                <img src="images/kumakerspace_space3.jpg" alt="">
+            </article>
+            <article class="intro1">
+                <img src="images/kumakerspace_space6.jpg" alt="">
+            </article>
+            <article class="intro3">
+                <img src="images/kumakerspace_space4.jpg" alt="">
+            </article>
+            </section>
+            <section class="contact-section">
+                <div class="container">
+                    <h2 class="sec-tit">CONTACT</h2>
+                    <div class="form-box">
+                        <form action="" method="">
+                            <fieldset class="cfixed">
+                                <legend class="blind">CONTACT US</legend>
+                                <div class="form">
+                                    <label for="name" class="blind">name</label>
+                                    <input type="text" id="name" placeholder="Name">
+                                    <label for="phone" class="blind">phone</label>
+                                    <input type="tel" id="phone" placeholder="Phone">
+                                    <label for="email" class="blind">email</label>
+                                    <input type="email" id="email" placeholder="Email Address">
+                                </div>
+                                <div class="textarea">
+                                    <label for="message" class="blind">message</label>
+                                    <textarea name="message" id="message" placeholder="Message"></textarea>
+                                </div>
+                            </fieldset>
+                            <div class="send-btn">
+                                <button>SEND MASSAGE</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </section>
+        <footer class="footer">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d790.4519002085414!2d127.02549282924325!3d37.5831479987372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbcb104e884ed%3A0x82d45b93eeb4ed6d!2z6rOg66Ck64yA7ZWZ6rWQIOywveydmOq0gA!5e0!3m2!1sko!2skr!4v1651492301040!5m2!1sko!2skr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <p class="copyright">Korea University</p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ project_3/product.php
@@ -0,0 +1,169 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+<title>고려대학교 KU메이커스페이스</title>
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/default.css">
+<link rel="shortcut icon" href="images/ku_logo.png">
+<!-- <link rel="apple-touch-icon-precomposed" href="images/favicon/indigo-touch-icon.png"> -->
+<link rel="apple-touch-icon-precomposed" href="images/ku_logo.png">
+<script src="js/jquery.min.js"></script>
+<script src="js/indigo.min.js"></script>
+</head>
+<body>
+    <div id="wrap">
+        <header class="header cfixed">
+            <h1 class="logo"><a href="index.php">KU Makerspace</a></h1>
+            <nav>
+                <ul class="gnb">
+                    <li><a href="introduction.php">기관 소개</a></li>
+                    <li><a href="education.php">장비교육</a></li>
+                    <li><a href="reservation.php">장비예약</a></li>
+                    <li><a href="notice.php">공지사항</a></li>
+                    <li><a href="freeboard.php">자유 게시판</a></li>
+                    <li><a href="product.php">대회 작품들</a></li>
+                    <li><a href="login/login_page.php">회원가입/로그인</a></li>
+                </ul>
+            </nav>
+            <span class="menu-toggle-btn">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
+        </header>
+        <!-- <article class="slider">
+            <img src="images/kumakerspace_space.jpg" alt="">
+        </article> -->
+        <section class="content">
+            <section class="display-section">
+                <div class="container">
+                    <h2 class="sec-tit">작품 소개</h2>
+                    <p class="desc">딥러닝 NeRF 기술을 활용하여 3D로 실감나게 구현된 작품들!<br>고려대학교 KU메이커스페이스에서 만들어진 작품들을 자유롭게 구경해보세요.</p>
+                </div>
+            </section>
+            <section class="work-section cfixed">
+                <ul class="work-list">
+                    <li>
+                        <a href="product1.php">
+                            <div class="info">
+                                <!-- Number 1 -->
+                                <h3>I am GROOT!</h3>
+                                <span>3D 프린터를 활용한 제작</span>
+                            </div>
+                            <img src="images/fig1_groot.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="product2.php">
+                            <div class="info">
+                                <!-- Number 2 -->
+                                <h3>월E</h3>
+                                <span>나만의 영화 캐릭터 만들기</span>
+                            </div>
+                            <img src="images/fig4_walle.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="product3.php">
+                            <div class="info">
+                                <!-- Number 3 -->
+                                <h3>부엉이</h3>
+                                <span>목재 작품 만들기</span>
+                            </div>
+                            <img src="images/fig3_owl.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="product4.php">
+                            <div class="info">
+                                <!-- Number 4 -->
+                                <h3>회색 토끼</h3>
+                                <span>나만의 토끼 만들기</span>
+                            </div>
+                            <img src="images/fig9_gr_2.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="product5.php">
+                            <div class="info">
+                                <!-- Number 5 -->
+                                <h3>어몽어스</h3>
+                                <span>3D 프린터를 활용한 제작</span>
+                            </div>
+                            <img src="images/fig5_amongus.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="product6.php">
+                            <div class="info">
+                                <!-- Number 6 -->
+                                <h3>파란색 성</h3>
+                                <span>나만의 성 만들기</span>
+                            </div>
+                            <img src="images/fig6_blue_shinjun.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="product7.php">
+                            <div class="info">
+                                <!-- Number 7 -->
+                                <h3>노란색 피카츄</h3>
+                                <span>피카 피카 피카츄</span>
+                            </div>
+                            <img src="images/fig7_pikachu.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="product8.php">
+                            <div class="info">
+                                <!-- Number 8 -->
+                                <h3>보라색 몬스터</h3>
+                                <span>귀여운 나만의 몬스터 만들기</span>
+                            </div>
+                            <img src="images/fig8_purple.png" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <!-- <section class="display-section">
+                <article class="slider">
+                    <img src="images/fig1_groot.png" alt="">
+                </article>
+            </section> -->
+            <section class="contact-section">
+                <div class="container">
+                    <h2 class="sec-tit">CONTACT</h2>
+                    <div class="form-box">
+                        <form action="" method="">
+                            <fieldset class="cfixed">
+                                <legend class="blind">CONTACT US</legend>
+                                <div class="form">
+                                    <label for="name" class="blind">name</label>
+                                    <input type="text" id="name" placeholder="Name">
+                                    <label for="phone" class="blind">phone</label>
+                                    <input type="tel" id="phone" placeholder="Phone">
+                                    <label for="email" class="blind">email</label>
+                                    <input type="email" id="email" placeholder="Email Address">
+                                </div>
+                                <div class="textarea">
+                                    <label for="message" class="blind">message</label>
+                                    <textarea name="message" id="message" placeholder="Message"></textarea>
+                                </div>
+                            </fieldset>
+                            <div class="send-btn">
+                                <button>SEND MASSAGE</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </section>
+        <footer class="footer">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d790.4519002085414!2d127.02549282924325!3d37.5831479987372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbcb104e884ed%3A0x82d45b93eeb4ed6d!2z6rOg66Ck64yA7ZWZ6rWQIOywveydmOq0gA!5e0!3m2!1sko!2skr!4v1651492301040!5m2!1sko!2skr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <p class="copyright">Korea University</p>
+        </footer>
+    </div>
+</body>
+</html>
